d32809306a65c4e0ddee3628b83a58e9de0ffcde
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-03-29 15:16:42 +0300

    splitting move_ants

--- sources/move_ants.c
+++ sources/move_ants.c
@@ -6,36 +6,30 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/28 10:50:09 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/03/28 20:33:19 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/03/29 15:16:08 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-static void make_moves(t_lem_in *anthill, t_room *tmp)
+static void move(t_lem_in *anthill, t_room *tmp)
 {
-    while (1)
+	while (tmp)
 	{
-		tmp = anthill->end;
-		if (tmp->ant_here != NULL && tmp->ant_here->next == NULL)
-			return ;
-		while (tmp)
+		if (tmp->prev && tmp->prev->ant_here && !tmp->ant_here) //no ants at current room and ants at prev
 		{
-			if (tmp->prev && tmp->prev->ant_here && !tmp->ant_here)
-			{
-				tmp->ant_here = tmp->prev->ant_here;
-				MOVE(tmp->ant_here->name, tmp->name);
-			}
-			else if (tmp->ant_here)
-			{
-				tmp->ant_here = tmp->ant_here->next;
-				if (tmp != anthill->start && tmp->ant_here)
-					MOVE(tmp->ant_here->name, tmp->name);
-			}
-			tmp = tmp->prev;
+			tmp->ant_here = tmp->prev->ant_here; //swithches ant from previous room to point current room
+			MOVE(tmp->ant_here->name, tmp->name); //print this move
 		}
-		ft_printf("\n");
+		else if (tmp->ant_here) //ant at the current room or (previous room)
+		{
+			tmp->ant_here = tmp->ant_here->next; //switches ant to next ant (L1 -> L2)
+			if (tmp != anthill->start && tmp->ant_here) //if current is not start AND there is ants left
+				MOVE(tmp->ant_here->name, tmp->name);
+		}
+		tmp = tmp->prev;
 	}
+		
 }
 
 void	move_ants(t_lem_in *anthill)
@@ -44,6 +38,13 @@ void	move_ants(t_lem_in *anthill)
 
     tmp = NULL;
     anthill->start->ant_here = anthill->ant_lst;
-    anthill->end->prev = anthill->paths->second_last;
-    make_moves(anthill, tmp);
+    anthill->end->prev = anthill->paths->next->second_last;
+    while (1)
+	{
+		tmp = anthill->end; //this will only track one path at the time untill all ants get to end
+		if (tmp->ant_here != NULL && tmp->ant_here->next == NULL) //ant_here pointing to last ant at the end
+			return ;
+		move(anthill, tmp); //one round
+		ft_printf("\n");
+	}
 }
