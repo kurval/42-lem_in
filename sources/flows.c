24a11008c7c4b3d02d90b1862557cd259b83e7b8
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-04-12 17:35:20 +0300

    huge refactoring

--- sources/flows.c
+++ sources/flows.c
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 13:01:46 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/04/12 16:34:55 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/04/12 17:33:09 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,6 @@ int		check_start_flow(t_lem_in *anthill)
 		if (anthill->flow[room->id][connects->room->id] == -1 ||\
 				!anthill->flow[room->id][connects->room->id])
 			return (1);
-		connects = connects->next;
-	}
-	return (0);
-}
-
-int		check_neg_flow(t_lem_in *anthill, t_room *room, t_room *parent)
-{
-	t_connect *connects;
-
-	connects = room->connections;
-    if (room->checked == NEG)
-        return (0);
-	while (connects)
-	{
-        if (connects->room != parent && connects->room->checked != VISITED)
-        {
-            if (anthill->flow[room->id][connects->room->id] == -1 ||\
-            !anthill->flow[room->id][connects->room->id])
-                return (1);
-        }
 		connects = connects->next;
 	}
 	return (0);
